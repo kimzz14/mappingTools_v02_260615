d2559504475d156b50b061f8b42cdd238cc8bde8
kimzz14 <kimzz1004@gmail.com>
2026-06-15 14:52:15 +0900

    update

--- batch/parallel.pre.sh
+++ batch/parallel.pre.sh
@@ -1,44 +1,53 @@
+############################################################################################
 prefix=$1
-jobSize=$2
-
+parallelN=$2
+############################################################################################
+# ----------------------------
+# Check arguments
+# ----------------------------
 if [ -z ${prefix} ]; then
     echo "prefix is empty."
-    echo "bash ./pipe/parallel.pre.sh run0001 24"
+    echo "bash ./batch/parallel.pre.sh run0001 24"
     exit 1
 fi
 
-if [ -z ${jobSize} ]; then
-    echo "jobSize is empty."
-    echo "bash ./pipe/parallel.pre.sh run0001 24"
+if [ -z ${parallelN} ]; then
+    echo "parallelN is empty."
+    echo "bash ./batch/parallel.pre.sh run0001 24"
     exit 1
 fi
 
+# ----------------------------
+# Set paths
+# ----------------------------
+inputFile="${prefix}.sh"
 workDir="./jobs.${prefix}.parallel"
+parallelScript="${prefix}.parallel.sh"
 
 # Check if the folder exists and exit if it does
 if [ -d "$workDir" ]; then
-    echo "The folder already exists. Exiting the program."
+    echo "The folder already exists.: $workDir"
     exit 1
 fi
 
-# Create working directories
 mkdir -p "$workDir"
-
-# Initialize the parallel execution script
-parallelScript="${prefix}.parallel.sh"
 > "$parallelScript"
 
-# 입력 파일 줄 단위 처리
+#create jobFile
 lineCount=0
-jobID=0
 while IFS= read -r line; do
-    if (( lineCount % jobSize == 0)); then
-        jobID=$((lineCount / jobSize))
+    if (( lineCount % parallelN == 0)); then
+        jobID=$((lineCount / parallelN))
         jobFile="$workDir/job_${jobID}"
         > "$jobFile"
-        echo "bash ./batch/parallel.sh $prefix $jobID" >> "$parallelScript"
+        echo "bash $jobFile" >> "$parallelScript"
     fi
-    echo "$line" >> "$jobFile"
+    
+    echo "$line &" >> "$jobFile"
     
     lineCount=$((lineCount + 1))
-done < "${prefix}.sh"
+done < "$inputFile"
+
+for jobFile in "$workDir"/job_*; do
+    echo "wait" >> "$jobFile"
+done
